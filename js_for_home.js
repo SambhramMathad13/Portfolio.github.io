b3979ca2002c10f5ebb1f57d8839ddd2a98c6e09
Sambhram Matahd <125111476+SambhramMathad13@users.noreply.github.com>
2023-02-13 04:55:22 -1000

    Add files via upload

---
+++ js_for_home.js
@@ -0,0 +1,20 @@
+let str="Hi I'm Sambhram Mathad...   "
+    var typed = new Typed(".hi", {
+            strings: [str],
+            typeSpeed: 70,
+            backSpeed: 100,
+            // loop: true
+        })
+        let st="Computer science Engineering and part time developer..."
+    var typed = new Typed(".what", {
+            strings: [st],
+            typeSpeed: 65,
+            backSpeed: 100,
+            // loop: true
+        })
+    var typed = new Typed(".moreinfo", {
+        strings: ["love coding..."," love playing footbal..."," love listning music..."],
+            typeSpeed: 65,
+            backSpeed: 100,
+            loop: true
+        })
